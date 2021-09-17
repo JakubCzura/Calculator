8ff1635c3c787c2bd74b5e6d3e46525017562b61
Jakub Czura <czurajakub@gmail.com>
2021-09-17 17:14:08 +0200

    ok

---
+++ ViewModel/Commands/EqualsButtonCommand.cs
@@ -0,0 +1,48 @@
+﻿using Calculator.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Calculator.ViewModel.Commands
+{
+    public class EqualsButtonCommand : ICommand
+    {
+        public EqualsButtonCommand(CalculatorVM calculatorVM, Result result)
+        {
+            CalculatorVM = calculatorVM;
+            Result = result;
+        }
+
+        CalculatorVM CalculatorVM { get; set; }
+
+        Result Result { get; set; }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        double fullResultDouble = 0;
+
+        public bool CanExecute(object parameter)
+        {
+            string fullResult = parameter as string;
+
+            if (fullResult != null && fullResult.Length > 0)
+            {
+                if (double.TryParse(parameter.ToString(), out fullResultDouble) == true && fullResultDouble >= 0 && double.IsInfinity(fullResultDouble) == false)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            
+        }
+    }
+}
